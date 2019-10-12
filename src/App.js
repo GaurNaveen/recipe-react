43950fb7dc873daa01b103d467995798e706bf92
Naveen Gaur <naveengaur800@gmail.com>
2019-10-12 16:42:44 +1100

    Web app can now connect to the API and get the data.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { get } from 'https';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = '85fae3d6';
+  const APP_KEY = 'aada471a9382fd5f84e236ad5a875e92';
+
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+
+
+  //Create a state here
+  const [recipes,setRecipies]  = useState([]);
+
+  // This is the function that will run everytime(useEffect)
+  useEffect(() => {
+    getRecipes();
+  }, []); // [counter] is the second argument. If counter state changes then it will execute this function.
+
+  // Create a function that will be used to make async calls and fetch recipes from the API.
+  const getRecipes = async () => {
+    const response = await fetch(exampleRequest);
+    const data = await response.json();
+    setRecipies(data.hits);
+  }
+
+  return(
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input type='text' className='search-bar'/>
+        <button type='submit' className='search-button'>Search</button>
+      </form>
     </div>
+
   );
 }
 
